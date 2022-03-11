bf6a62e53477e24b619a7d577e11187540bbc271
Salma Akter <salmaakter@Salmas-MacBook-Pro.local>
2022-03-11 22:22:00 +0600

    first commit

---
+++ Project Source Code/01. Website/assests/JS/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function($){
+        $(".case-studies-carousel").owlCarousel({
+            items: 3,
+            margin: 30,
+            loop: true,
+            dots: true,
+            nav: false,
+        });
+
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            dots: true,
+            nav: false,
+        });
+    });
+
+    jQuery(window).load(function(){
+
+    });
+
+}(jQuery));
